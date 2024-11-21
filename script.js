4e1a7abc784e24ba259934dbeb77cabff10c2083
Subrata <subrata@iastate.edu>
2024-11-21 12:59:04 -0600

    Website little changed

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function showTab(event, tabId) {
     selectedTab.classList.add('active');
     event.target.classList.add('active');
 }
+
+
+// let slideIndex = 0;
+
+// function showSlides() {
+//     let slides = document.getElementsByClassName("mySlides");
+//     for (let i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";  
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {
+//         slideIndex = 1;    
+//     }
+//     slides[slideIndex - 1].style.display = "block";  
+//     setTimeout(showSlides, 5000); // Change image every 2 seconds
+// }
+
+// showSlides(); // Call the function to start the slideshow
